ae9d1f879726d3e831b02e412b6bac4f1b8b9a77
Mikhailer <eremix2017@yandex.ru>
2026-06-08 17:35:44 +0300

    some changes

--- main.py
+++ main.py
@@ -180,4 +180,4 @@ async def start_scan(tm_numbers: str, background_tasks: BackgroundTasks, manual_
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
